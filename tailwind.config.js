81a43c17594075b4bcc233ff875a89da90792da7
Yaroslav <6715414@gmail.com>
2024-01-02 14:31:43 +0200

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        overlay: 'rgba(0,0,0, 0.6)'
+      },
+    },
+  },
+  plugins: [],
+}
+
